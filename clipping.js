35c6eed8467b0d24f198cdd782b1b268748e083e
walidlec <walid.lec@gmail.com>
2023-04-15 17:34:44 +0200

    reset button and things that I forgor about

--- clipping.js
+++ clipping.js
@@ -7,6 +7,7 @@ const { spawn } = require('child_process');
 const fs = require('fs');
 const { time } = require("console");
 const { getVideoDurationInSeconds } = require('get-video-duration');
+const { ButtonBuilder } = require("@discordjs/builders");
 
 const client = new discord.Client({
     intents: [
@@ -29,7 +30,7 @@ client.once('ready', () => {
 });
 
 // Replace VIDEO_URL with the URL of the YouTube video you want to clip
-let VIDEO_URL = 'https://youtu.be/r-g3_EYi814';
+let VIDEO_URL = '';
 
 
 let audio_stream;
@@ -60,8 +61,50 @@ let cut;
 let timestamptext;
 let clipduration;
 let clipdurationtext;
+let enddelay;
+let enddelaytext;
+let recordtime;
+let recordWarning;
 
 console.log(Math.floor(new Date(Date.now()).getTime() / 1000))
+
+const btnReset = new ActionRowBuilder()
+    .addComponents(
+        new ButtonBuilder()
+            .setCustomId('reset')
+            .setLabel('Reset')
+            .setStyle(ButtonStyle.Primary),
+    )
+
+
+client.on('interactionCreate', async interaction => {
+    
+    if(interaction.isButton()){
+        switch(interaction.customId){
+            case 'reset':
+                stream.close()
+                ytdl(VIDEO_URL).pipe(stream = fs.createWriteStream('video.mp4'))
+                recordtime = '<t:' + Math.floor(new Date(Date.now()).getTime() / 1000) + ':R>'
+                interaction.message.edit({content: 'It has been ' + recordtime + 'since the recording started, reset it to prevent having to encode a heavy file', components: [btnReset]})
+                interaction.reply({content: 'Recording got reset!', ephemeral: true})
+            break;
+        }
+    }
+
+    switch(interaction.commandName){ 
+        case 'reset':
+            stream.close()
+            ytdl(VIDEO_URL).pipe(stream = fs.createWriteStream('video.mp4'))
+            interaction.channel.send('<t:' + Math.floor(new Date(Date.now()).getTime() / 1000) + ':R>')
+        break;
+        
+        case 'recut':
+
+        break;
+    }
+
+});
+
 
 client.on('messageCreate', async message => {
     if(message.author.bot == true){return}
@@ -79,6 +122,13 @@ client.on('messageCreate', async message => {
                 
                 clipping.on('close', async () => {
                     ytdl(VIDEO_URL).pipe(stream = fs.createWriteStream('video.mp4'))
+                    
+                    recordtime = '<t:' + Math.floor(new Date(Date.now()).getTime() / 1000) + ':R>'
+                    recordWarning = setTimeout(function () {
+                        message.channel.send({ content: 'It has been ' + recordtime + 'since the recording started, reset it to prevent having to encode a heavy file', components: [btnReset]})
+    
+                    }, 10000)
+                    
                     console.log("lol")
                     
                     if(args.length < 2){
@@ -117,26 +167,55 @@ client.on('messageCreate', async message => {
                 ytdl(VIDEO_URL).pipe(stream = fs.createWriteStream('video.mp4'))
                 message.react("✅")
                 message.channel.send('<t:' + Math.floor(new Date(Date.now()).getTime() / 1000) + ':R>')
+                recordtime = '<t:' + Math.floor(new Date(Date.now()).getTime() / 1000) + ':R>'
+                recordWarning = setTimeout(function () {
+                    message.channel.send({ content: 'It has been ' + recordtime + 'since the recording started, reset it to prevent having to encode a heavy file', components: [btnReset]})
+
+                }, 10000)
+            break;
+            case 'stop':
+                if(stream != undefined){
+                    stream.close()
+                }
             break;
             case 'record':
+                if(VIDEO_URL == ''){ return message.channel.send("No stream got set up")}
                 ytdl(VIDEO_URL).pipe(stream = fs.createWriteStream('video.mp4'))
                 message.react("✅")
+                recordtime = '<t:' + Math.floor(new Date(Date.now()).getTime() / 1000) + ':R>'
+                recordWarning = setTimeout(function () {
+                    message.channel.send({ content: 'It has been ' + recordtime + 'since the recording started, reset it to prevent having to encode a heavy file', components: [btnReset]})
+
+                }, 10000)
             break;
             case 'stream':
                 if(args.length < 2) { return }
                 VIDEO_URL = args[1];
                 ytdl(VIDEO_URL).pipe(stream = fs.createWriteStream('video.mp4'))
                 message.react("✅")
+                recordtime = '<t:' + Math.floor(new Date(Date.now()).getTime() / 1000) + ':R>'
+                recordWarning = setTimeout(function () {
+                    message.channel.send({ content: 'It has been ' + recordtime + 'since the recording started, reset it to prevent having to encode a heavy file', components: [btnReset]})
+
+                }, 10000)
             break;
             case 'recut':
-                if(args.length < 2){
-                    clipduration = 20;
-                }else{
+                enddelay = 0;
+                if(args.length > 2){
                     clipduration = parseInt(args[1], 10);
+                    enddelay = parseInt(args[2], 10);
+                }else{
+                    if(args.length < 2){
+                        clipduration = 20;
+                    }else{
+                        clipduration = parseInt(args[1], 10);
+                    }
                 }
-                timestamp = Math.floor(await getVideoDurationInSeconds('render.mp4') - clipduration);
+                timestamp = Math.floor(await getVideoDurationInSeconds('render.mp4') - clipduration - enddelay);
                 timestamptext = timestamp.toString()
                 clipdurationtext = clipduration.toString()
+                enddelaytext = enddelay.toString()
+
                 console.log(timestamp)
                 
                 let args2 = [
@@ -150,7 +229,7 @@ client.on('messageCreate', async message => {
                     //'-c', 'copy',
                     'render' + clipdurationtext + 's.mp4' // Output file
                   ];
-                sleep(1000)
+                sleep(500)
 
                 cut = spawn(ffmpeg, args2)
                 cut.on('close', () => {
